afd01c9e6ded9987bca6d90abee07c287bc7ab82
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-03-27 09:06:41 +0100

    fix: create function now works

--- pizzeria-crud/src/main/java/org/lessons/java/pizzeria/pizzeria_crud/controller/PizzaController.java
+++ pizzeria-crud/src/main/java/org/lessons/java/pizzeria/pizzeria_crud/controller/PizzaController.java
@@ -7,10 +7,15 @@ import org.lessons.java.pizzeria.pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -39,6 +44,19 @@ public class PizzaController {
     public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
         return "pizzas/create";
+    }
+    
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+        // controllo errori
+        if (bindingResult.hasErrors()) {
+            return "pizzas/create";
+        }
+
+        // salvataggio con la repository
+        repository.save(formPizza);
+        return "redirect:/pizzas";
     }
     
 }
